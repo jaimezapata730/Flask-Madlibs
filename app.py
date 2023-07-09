ed23539602cd61a8aa49418369ba5d6530f34cbb
jaime zapata <jaimezapata730@gmail.com>
2023-07-09 14:53:57 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import stories
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret'
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def ask_story():
+    """Show list-of-stories form."""
+
+    return render_template('select-story.html', stories=stories.values())
+
+
+@app.route('/questions')
+def ask_questions():
+    """Form to make questions."""
+    story_id = request.args['story_id']
+    story = stories[story_id]
+    prompts = story.prompts
+    return render_template('questions.html', story_id=story_id, title=story.title, prompts=prompts)
+
+@app.route('/story')
+def s_story():
+    """Show story result."""
+    story_id = request.args['story_id']
+    story = stories[story_id]
+    text = story.generate(request.args)
+    return render_template('story.html', title=story.title, text=text)
